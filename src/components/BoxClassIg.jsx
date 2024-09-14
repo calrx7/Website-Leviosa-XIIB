32d41f8c7b8ba9e782704d1a6992b7800acecdd2
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-14 21:23:31 +0800

    Update fix

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -7,7 +7,7 @@ const BoxClassIg = () => {
 					<img src="/next.png" alt="" className="h-4 w-4" />
 				</div>
 				<h1 className="text-white text-lg font-semibold pr-3 mt-3 absolute bottom-10">
-					Class Instagram
+					Instagram Kelas
 				</h1>
 				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
 			</a>
