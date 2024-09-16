3247f8420e549b3e59918a7ad61576fe8dfde793
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-16 21:27:17 +0800

    Update BorderStruktur.jsx

--- src/components/BorderStruktur.jsx
+++ src/components/BorderStruktur.jsx
@@ -7,7 +7,7 @@ const BorderStruktur = (props) => {
 	return (
 		<div className="flex flex-col justify-center items-center"> 
 			<div className="text-white text-sm mb-1">{Jabatan}</div>
-			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full" onContextMenu={disableRightClick} onDragStart={disableDownload} onTouchStart={disableDownload} />} 
+			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full" />} 
 			<div className={`bg-white text-black rounded-3xl text-[0.8rem] px-2 py-[0.30rem] text-center font-semibold`} style={{ width: Width }}>
 				{Nama}
 			</div>
