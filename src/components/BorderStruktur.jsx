b29e861849d492b0f755bbdda7065817306a131e
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-16 22:00:27 +0800

    Update BorderStruktur.jsx

--- src/components/BorderStruktur.jsx
+++ src/components/BorderStruktur.jsx
@@ -3,11 +3,11 @@ const BorderStruktur = (props) => {
 	const Nama = props.Nama;
 	const Width = props.Width;
 	const ImgUrl = props.ImgUrl;  // Menambahkan properti untuk URL gambar
-	
+
 	return (
 		<div className="flex flex-col justify-center items-center"> 
 			<div className="text-white text-sm mb-1">{Jabatan}</div>
-			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full className="w-auto h-10}" />} 
+			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full" />} 
 			<div className={`bg-white text-black rounded-3xl text-[0.8rem] px-2 py-[0.30rem] text-center font-semibold`} style={{ width: Width }}>
 				{Nama}
 			</div>
