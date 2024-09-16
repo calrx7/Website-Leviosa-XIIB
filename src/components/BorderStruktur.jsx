c2fcfffc89290f603806e7095df4f56effa0941b
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-16 21:58:21 +0800

    Update BorderStruktur.jsx

--- src/components/BorderStruktur.jsx
+++ src/components/BorderStruktur.jsx
@@ -4,17 +4,10 @@ const BorderStruktur = (props) => {
 	const Width = props.Width;
 	const ImgUrl = props.ImgUrl;  // Menambahkan properti untuk URL gambar
 	
-	const disableRightClick = (e) => {
-		e.preventDefault();
-	};
-	const disableDownload = (e) => {
-		e.preventDefault();
-	};
-
 	return (
 		<div className="flex flex-col justify-center items-center"> 
 			<div className="text-white text-sm mb-1">{Jabatan}</div>
-			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full className="w-auto h-10 onDragStart={disableDownload} onTouchStart={disableDownload}" />} 
+			{ImgUrl && <img src={ImgUrl} alt={Nama} className="w-[80px] h-[80px] object-cover mb-2 rounded-full className="w-auto h-10}" />} 
 			<div className={`bg-white text-black rounded-3xl text-[0.8rem] px-2 py-[0.30rem] text-center font-semibold`} style={{ width: Width }}>
 				{Nama}
 			</div>
