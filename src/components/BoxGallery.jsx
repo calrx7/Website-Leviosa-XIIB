68a77c201985c5132d20fec43b2e2e3e02b647a4
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-16 22:00:57 +0800

    Update BoxGallery.jsx

--- src/components/BoxGallery.jsx
+++ src/components/BoxGallery.jsx
@@ -1,17 +1,9 @@
 const BoxGallery = () => {
-
-    const disableRightClick = (e) => {
-		e.preventDefault();
-	};
-	const disableDownload = (e) => {
-		e.preventDefault();
-	};
-	
 	return (
 		<div id="BoxGallery">
 			<div className="flex justify-between">
-				<img src="/upload.png" alt="" className="w-auto h-10 onDragStart={disableDownload} onTouchStart={disableDownload}" />
-				<img src="/next.png" alt="" className="h-5 w-5 onDragStart={disableDownload} onTouchStart={disableDownload}" />
+				<img src="/upload.png" alt="" className="w-auto h-10" />
+				<img src="/next.png" alt="" className="h-5 w-5" />
 			</div>
 
 			<h1 className="text-white text-xl pr-3  mt-3">Class Gallery</h1>
@@ -20,4 +12,3 @@ const BoxGallery = () => {
 }
 
 export default BoxGallery
-	
