1bd8c82a148d555c0e51637afe9cf60b095ca5be
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-16 21:41:18 +0800

    Update fix

--- src/components/BoxGallery.jsx
+++ src/components/BoxGallery.jsx
@@ -2,8 +2,8 @@ const BoxGallery = () => {
 	return (
 		<div id="BoxGallery">
 			<div className="flex justify-between">
-				<img src="/upload.png" alt="" className="w-auto h-10" />
-				<img src="/next.png" alt="" className="h-5 w-5" />
+				<img src="/upload.png" alt="" className="w-auto h-10 onContextMenu={disableImageDownload} onDragStart={disableImageDownload}" />
+				<img src="/next.png" alt="" className="h-5 w-5 onContextMenu={disableImageDownload} onDragStart={disableImageDownload}" />
 			</div>
 
 			<h1 className="text-white text-xl pr-3  mt-3">Class Gallery</h1>
