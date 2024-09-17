e8eb63bd2b7cca8191000d1db47d7729c58080c0
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-17 09:14:12 +0800

    Update BoxGallery.jsx

--- src/components/BoxGallery.jsx
+++ src/components/BoxGallery.jsx
@@ -1,9 +1,33 @@
 const BoxGallery = () => {
 	return (
+	
+	const disableRightClick = (e) => {
+		e.preventDefault();
+	};
+
+	// Fungsi untuk menonaktifkan dragging dan long-press
+	const disableDownload = (e) => {
+		e.preventDefault();
+	};
+
 		<div id="BoxGallery">
 			<div className="flex justify-between">
-				<img src="/upload.png" alt="" className="w-auto h-10" />
-				<img src="/next.png" alt="" className="h-5 w-5" />
+				<img 
+					src="/upload.png" 
+					alt="" 
+					className="w-auto h-10" 
+					onContextMenu={disableRightClick} // Disable klik kanan (PC)
+					onDragStart={disableDownload} // Disable dragging (PC)
+					onTouchStart={disableDownload} // Disable long-press (Mobile)
+				/>
+				<img 
+					src="/next.png" 
+					alt="" 
+					className="h-5 w-5"
+					onContextMenu={disableRightClick} // Disable klik kanan (PC)
+					onDragStart={disableDownload} // Disable dragging (PC)
+					onTouchStart={disableDownload} // Disable long-press (Mobile)
+				/>
 			</div>
 
 			<h1 className="text-white text-xl pr-3  mt-3">Class Gallery</h1>
@@ -12,3 +36,4 @@ const BoxGallery = () => {
 }
 
 export default BoxGallery
+				
