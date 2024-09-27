8a38ddc2cbb3a6d3caa37e5d61f9b6b13332854a
CaloraX <149755932+calrx7@users.noreply.github.com>
2024-09-28 07:13:28 +0800

    Update vite.conf

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Pages from 'vite-plugin-pages';
 import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
+    Pages(),
     Sitemap({
-      hostname: 'https://leviosaxiib.vercel.app', // Ganti dengan domain Anda
+      hostname: 'https://your-domain.com', // Ganti dengan domain Anda
     }),
   ],
 });
