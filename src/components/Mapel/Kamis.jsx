ff312f375336d1cfb49e89d0d3fcdf574112d92a
CaloraX <149755932+calrx7@users.noreply.github.com>
2025-02-18 21:08:49 +0800

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,14 +5,14 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">B.Arab</div>
+				<div className="w-[50%] text-base  font-medium">B. Arab</div>
 				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+				<div className="w-[50%] text-base  font-medium">PJOK</div>
 				<div className="flex justify-center items-center text-sm">09.40-10.00</div>
 			</div>
 			<div
@@ -26,7 +26,7 @@ const Kamis = () => {
 				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+				<div className="w-[50%] text-base  font-medium">PJOK</div>
 				<div className="flex justify-center items-center text-sm">11.20-12.20</div>
 			</div>
 			<div
