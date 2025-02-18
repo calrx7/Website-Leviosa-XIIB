489927ea7fa3ca58eee1aeca28d595650a2e14db
CaloraX <149755932+calrx7@users.noreply.github.com>
2025-02-18 21:04:29 +0800

    Update Selasa.jsx
    Mapel Revisi

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,11 +2,11 @@ const Selasa = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">PJOK</div>
+					<div className="w-[50%] text-base  font-medium">Matematika TL</div>
 					<div className="flex justify-center items-center text-sm">07.00-08.20</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Matematika TL</div>
+					<div className="w-[50%] text-base  font-medium">Bhs. Inggris</div>
 					<div className="flex justify-center items-center text-sm">08.20-09.40</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,7 +14,7 @@ const Selasa = () => {
 					<div className="flex justify-center items-center text-sm">09.40-10.00</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Matematika TL</div>
+					<div className="w-[50%] text-base  font-medium">Bhs. Inggris</div>
 					<div className="flex justify-center items-center text-sm">10.00-11.20</div>
 				</div>
 
@@ -27,7 +27,7 @@ const Selasa = () => {
 					<div className="flex justify-center items-center text-sm">12.20-13.40</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Sejarah</div>
+					<div className="w-[50%] text-base font-medium">Biologi</div>
 					<div className="flex justify-center items-center text-sm">13.40-15.00</div>
 				</div>
         </>
