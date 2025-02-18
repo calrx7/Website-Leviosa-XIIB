de60a808881e7e445554b25a719c8ff681a520f6
CaloraX <149755932+calrx7@users.noreply.github.com>
2025-02-18 21:02:02 +0800

    Update Senin.jsx
    Mapel revisi

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -23,7 +23,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">12.20-13.20</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Biologi</div>
+					<div className="w-[50%] text-base font-medium">Sejarah</div>
 					<div className="flex justify-center items-center text-sm">13.20-14.40</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
